2ff21a685229900117c1e466163b31deea59124f
Caro <carolynatterbury@gmail.com>
2020-04-27 17:11:43 -0600

    officially normalizing data. saving as wav files in data/project_waves_norm/train

--- normalization.py
+++ normalization.py
@@ -6,20 +6,15 @@ import numpy as np
 
 def compute_mean(path):
     count = 0
-    sums = np.zeros(1400)
+    sums = 0
 
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
             path = os.path.join(root, name)
             samplerate, data = scipy.io.wavfile.read(path)
-
             if len(data.shape) > 1:
-                song_length=1400
-                for i in range(0, song_length):
-                    sums[i] += data[i,0]
-
-                count+=1
-
+                count+=len(data[:,0])
+                sums+= np.sum(data[:,0])
 
     return sums / count
 
@@ -27,7 +22,7 @@ def compute_mean(path):
 def compute_sd(path, avg):
 
     count = 0
-    sums = np.zeros(1400)
+    sums = 0
 
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
@@ -35,38 +30,52 @@ def compute_sd(path, avg):
             samplerate, data = scipy.io.wavfile.read(path)
 
             if len(data.shape) > 1:
-                song_length=1400
-                for i in range(0, song_length):
-                    sums[i] += math.pow((data[i,0] - avg[i]),2)
-                count+=1
+                count += len(data[:,0])
+                sums += np.sum(np.square(data[:,0] - avg))
 
     variance = sums/(count-1)
     return np.sqrt(variance)
 
-def normalize_data(path, avg, std):
-    with open('data/normalized_data.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        for root, dirs, files in os.walk(path, topdown=False):
-            for name in files:
-                path = os.path.join(root, name)
-                samplerate, data = scipy.io.wavfile.read(path)
+def normalize_data(path, out_path, avg, std):
+    make_dirs()
 
-                if len(data.shape)>1:
-                    data_norm = (data[:1400,0] - avg) / std
-                    data_norm = np.concatenate(([name.split(".")[0]], data_norm))
-                    writer.writerow(data_norm)
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            path = os.path.join(root, name)
+            samplerate, data = scipy.io.wavfile.read(path)
+
+            if len(data.shape)>1:
+                data_norm = (data[:,0] - avg) / std
+                scipy.io.wavfile.write(out_path + name.split(".")[0], samplerate, data_norm)
+
+def make_dirs():
+    try:
+        os.makedirs("data/project_waves_norm")
+    except FileExistsError:
+        # directory already exists
+        pass
+
+    try:
+        os.makedirs("data/project_waves_norm/train")
+    except FileExistsError:
+        # directory already exists
+        pass
+
 
 if (__name__ == '__main__'):
 
     waves_path = 'data/project_waves/train/'
+    output_path = 'data/project_waves_norm/train/'
 
     print("computing mean...")
     avg = compute_mean(waves_path)
+    print(avg)
 
     print("computing standard deviation...")
     sd = compute_sd(waves_path,avg)
+    print(sd)
 
     print("normalizing data...")
-    normalize_data(waves_path, avg,sd)
+    normalize_data(waves_path, output_path, avg, sd)
 
     print("Done. Normalized data has been written to data/normalized_data.csv")
