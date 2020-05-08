8d5cd6eb48a136f36c4aac55e18a02835526cb25
Keira Haskins <keirahaskins26@gmail.com>
2020-05-08 14:36:35 -0600

    Modified predictions.py to make it runnable and added output csv.

--- predictions.py
+++ predictions.py
@@ -1,11 +1,38 @@
 import sys
 import csv
+import os
+import numpy as np
+import matplotlib.image as mpimg 
+import pickle
+
+# Dimensions of data being passed into network
+DIM_ROWS = 234 # max 277
+DIM_COLS = 200 # max 372
+DIM_CHANNELS = 3 # max 3
+TOTAL_INPUT_SIZE = DIM_ROWS*DIM_COLS*DIM_CHANNELS
+
+# NN Parameters
+
+EPOCHS = 30
+BATCH_SIZE = 150
+LEARNING_RATE = 0.001
+MOMENTUM = 0.9
+DECAY = 1e-6
+NESTEROV = True
+LOSS_FUNCTION = 'categorical_crossentropy'
+
+# Number of training data (remainder of data will go to testing)
+NUM_TRAINING = 1840
+LEAKY_RELU_ALPHA = 0.01
+
+USE_NEPTUNE = True
 
 def write_to_csv(names, predictions,out_path):
-    with open(out_path, 'w', newline='') as file:
+    #with open(out_path, 'w', newline='') as file:
+    with open(out_path, 'w') as file:
         writer = csv.writer(file)
         for i in range(0, len(names)):
-            writer.writerow([ names[i], predictions[i]])
+            writer.writerow([names[i], predictions[i]])
 
 def get_test_data(in_path):
     t_files = []
@@ -19,8 +46,9 @@ def get_test_data(in_path):
 
     data     = np.zeros((len(t_files),DIM_ROWS,DIM_COLS,DIM_CHANNELS))
 
-    for path in t_files:
-        image = mpimg.imread(path)
+    #for path in t_files:
+    for i in range(0, len(t_files)):
+        image = mpimg.imread(t_files[i])
         np_image = np.array(image)[:DIM_ROWS, :DIM_COLS, :DIM_CHANNELS]
         data[i] = np_image
 
@@ -43,12 +71,12 @@ def make_predictions(classifier, in_path, out_path):
     else:
         print("Incorrect classifier. Try again.")
 
-    write_to_csv(names,predictions,out_path)
+    write_to_csv(names, predictions, out_path)
 
 if __name__ == '__main__':
     # make predictions about data
 
-    predictions_path = '/predictions'
+    predictions_path = 'predictions'
 
     paths = [
         'project_spect/test/',
