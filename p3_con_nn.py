e078efebee04d71ce23668022cc7a851ee1076f1
Alyshia Nichole Bustos <abustos2436@unm.edu>
2020-05-09 16:08:47 -0600

    update

--- p3_con_nn.py
+++ p3_con_nn.py
@@ -59,53 +59,65 @@ class StridedNet:
 		model = Sequential()
 		inputShape = (height, width, depth)
 		chanDim = -1
-
+        print('====================================================================')
+        print('Starting CNN')
+        print('====================================================================')
 		# if we are using "channels first", update the input shape
 		# and channels dimension
 		if K.image_data_format() == "channels_first":
 			inputShape = (depth, height, width)
 			chanDim = 1
+            
         # our first CONV layer will learn a total of 16 filters, each
 		# Of which are 7x7 -- we'll then apply 2x2 strides to reduce
 		# the spatial dimensions of the volume
 		model.add(Conv2D(16, (7, 7), strides=(1, 1), padding="valid",
 			kernel_initializer=init, kernel_regularizer=reg,
 			input_shape=inputShape))
-
+        print('====================================================================')
+        print('Starting CNN_32')
+        print('====================================================================')
 		# here we stack two CONV layers on top of each other where
 		# each layerswill learn a total of 32 (3x3) filters
 		model.add(Conv2D(32, (7, 7), padding="same",
 			kernel_initializer=init, kernel_regularizer=reg))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(32, (7, 7), strides=(1, 1), padding="same",
+		model.add(Conv2D(32, (7, 7), strides=(1, 1), padding="valid",
 			kernel_initializer=init, kernel_regularizer=reg))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(Dropout(0.25))
+        print('====================================================================')
+        print('Starting CNN_64')
+        print('====================================================================')
         # stack two more CONV layers, keeping the size of each filter
 		# as 3x3 but increasing to 64 total learned filters
 		model.add(Conv2D(64, (5, 5), padding="same",
 			kernel_initializer=init, kernel_regularizer=reg))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(64, (5, 5), strides=(1, 1), padding="same",
+		model.add(Conv2D(64, (5, 5), strides=(1, 1), padding="valid",
 			kernel_initializer=init, kernel_regularizer=reg))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(Dropout(0.25))
-
+        print('====================================================================')
+        print('Starting CNN_128')
+        print('====================================================================')
 		# increase the number of filters again, this time to 128
 		model.add(Conv2D(128, (3, 3), padding="same",
 			kernel_initializer=init, kernel_regularizer=reg))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(128, (3, 3), strides=(1, 1), padding="same",
+		model.add(Conv2D(128, (3, 3), strides=(1, 1), padding="valid",
 			kernel_initializer=init, kernel_regularizer=reg))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(Dropout(0.25))
-        
+        print('====================================================================')
+        print('Starting CNN_512')
+        print('====================================================================')
         # fully-connected layer
 		model.add(Flatten())
 		model.add(Dense(512, kernel_initializer=init))
