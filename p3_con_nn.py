58ac2a9f13f8d12efb4f2ab43367c57715a6bd54
Alyshia Nichole Bustos <abustos2436@unm.edu>
2020-05-09 17:45:36 -0600

    new

--- p3_con_nn.py
+++ p3_con_nn.py
@@ -38,6 +38,7 @@ import csv
 DIM_ROWS = 100 # max 277
 DIM_COLS = 96 # max 372
 DIM_CHANNELS = 3 # max 3
+TOTAL_INPUT_SIZE = DIM_ROWS*DIM_COLS*DIM_CHANNELS
 
 EPOCHS = 30
 BATCH_SIZE = 50
@@ -61,7 +62,7 @@ class StridedNet:
         # initialize the model along with the input shape to be
         # "channels last" and the channels dimension itself
         model = Sequential()
-        inputShape = (height, width, depth)
+        #inputShape = (height, width, depth)
         chanDim = -1
         print('====================================================================')
         print('Starting CNN')
@@ -69,15 +70,15 @@ class StridedNet:
 		# if we are using "channels first", update the input shape
 		# and channels dimension
         if K.image_data_format() == "channels_first":
-            inputShape = (depth, height, width)
+            #inputShape = (depth, height, width)
             chanDim = 1
             
         # our first CONV layer will learn a total of 16 filters, each
 		# Of which are 7x7 -- we'll then apply 2x2 strides to reduce
 		# the spatial dimensions of the volume
-        model.add(Conv2D(16, (7, 7), strides=(1, 1), padding="valid",
+        model.add(Conv2D(16, (7, 7), strides=(1, 1), padding="same",
         kernel_initializer=init, kernel_regularizer=reg,
-        input_shape=inputShape))
+        input_shape=TOTAL_INPUT_SIZE))
         print('====================================================================')
         print('Starting CNN_32')
         print('====================================================================')
